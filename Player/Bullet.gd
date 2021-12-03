90c42aa194120a7c54b37e8baad9974e52fc45ca
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-03 16:39:05 +0100

    Janky bullets

---
+++ Player/Bullet.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+var speed = 500
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const Bullet = preload("res://Player/Bullet.tscn")
+
 export var ACCELERATION = 500
 export var MAX_SPEED = 200
 export var FRICTION = 500
@@ -7,6 +9,10 @@ export var FRICTION = 500
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	process_move(delta)
+	process_shoot(delta)
+
+func process_move(delta):
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -16,3 +22,15 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+
+func process_shoot(delta):
+	var input_vector = Input.get_vector("shoot_left", "shoot_right", "shoot_up", "shoot_down")
+	if input_vector != Vector2.ZERO:
+
+		var bullet_instance = Bullet.instance()
+		owner.add_child(bullet_instance)
+
+		var bullet_transform = transform
+		bullet_transform.x = transform.x + input_vector.normalized()
+		bullet_instance.transform = bullet_transform
+		

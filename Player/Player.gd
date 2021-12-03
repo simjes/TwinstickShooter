057a02671d5e27dfaf2c797924c0442487f5a912
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-03 14:47:17 +0100

    Remove unused variables

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,15 +7,12 @@ export var FRICTION = 500
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	print(input_vector)
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		var target_vector = input_vector + position
-		rotation = lerp_angle(rotation, (target_vector - position).normalized().angle(), 0.1)
+		rotation = lerp_angle(rotation, input_vector.normalized().angle(), 0.1)
 
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
-

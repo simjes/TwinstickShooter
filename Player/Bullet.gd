076c55b5d683974147a03b43109900b294b6ac60
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-04 15:22:12 +0100

    Refactor deleting rogue bullets

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -5,3 +5,6 @@ var speed = 300
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- World.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_BulletDespawn_body_entered(body):
-	body.queue_free()

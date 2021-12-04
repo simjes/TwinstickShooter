03d593b1047381f095fd7c23848533ebc724b9a6
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-04 23:30:58 +0100

    Fix spawn timer bug

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+onready var timer = $Timer
+
 export(float) var waitTime = 3
 export(String, FILE) var EnemyPath = null
 var spawnPoints = []
 
-func _on_Timer_timeout():
+func _ready():
+	spawn_enemy()
+	timer.start(waitTime)
+	
+func spawn_enemy():
 	var Enemy = load(EnemyPath)
 	var enemy = Enemy.instance()
 	get_tree().current_scene.add_child(enemy)
@@ -14,3 +20,7 @@ func _on_Timer_timeout():
 		enemy.position = spawnPoints[spawnIndex]
 	else:
 		enemy.position = global_position
+
+
+func _on_Timer_timeout():
+	spawn_enemy()

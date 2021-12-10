76bb5e9c984f908dd816c8e7ea276123ed0d6ccf
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-10 10:53:35 +0100

    Start menu

---
+++ UI/MainMenu.gd
@@ -0,0 +1,42 @@
+extends MarginContainer
+
+onready var Start = $HBoxContainer/VBoxContainer/MenuOptions/Start
+onready var Quit = $HBoxContainer/VBoxContainer/MenuOptions/Quit
+onready var menu_options = [Start, Quit]
+
+var hover_index = 0
+
+func _ready():
+	set_selected_option(hover_index)
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_up"):
+		var index = clamp(hover_index - 1, 0, menu_options.size())
+		hover_menu_option(index)
+		return
+		
+	if Input.is_action_just_pressed("ui_down"):
+		var index = clamp(hover_index + 1, 0, menu_options.size())
+		hover_menu_option(index)
+		return
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		select_menu_option()
+		
+func select_menu_option():
+	if hover_index == 0:
+		get_tree().change_scene("res://World.tscn")
+	
+	if hover_index == 1:
+		get_tree().quit()
+	
+func hover_menu_option(new_index):
+	reset_selected_option(hover_index)
+	hover_index = new_index
+	set_selected_option(hover_index)
+
+func reset_selected_option(index):
+	menu_options[index].add_color_override("font_color", Color.white)
+
+func set_selected_option(index):
+	menu_options[index].add_color_override("font_color", Color.aqua)

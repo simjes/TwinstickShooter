c85f2798017040890e955845f9a3f9fa0be45cc9
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2021-12-10 12:37:34 +0100

    Improve pause menu

--- PausePopup.gd
+++ PausePopup.gd
@@ -1,8 +1,15 @@
 extends Popup
 
-onready var Resume = $Resume
-onready var Restart = $Restart
-onready var Exit = $Exit
+onready var Resume = $CenterContainer/VBoxContainer/Resume
+onready var Quit = $CenterContainer/VBoxContainer/Quit
+
+onready var menu_options = [Resume, Quit]
+onready var max_index = menu_options.size() -1
+
+var hover_index = 0
+
+func _ready():
+	set_selected_option(hover_index)
 
 func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel") and !is_visible_in_tree():
@@ -14,10 +21,33 @@ func _input(_event):
 		hide()
 		get_tree().paused = false
 		return
+		
+	if Input.is_action_just_pressed("ui_up") and is_visible_in_tree():
+		var index = clamp(hover_index - 1, 0, max_index)
+		hover_menu_option(index)
+		
+	if Input.is_action_just_pressed("ui_down") and is_visible_in_tree():
+		var index = clamp(hover_index + 1, 0, max_index)
+		hover_menu_option(index)
+		
+	if Input.is_action_just_pressed("ui_accept") and is_visible_in_tree():
+		select_menu_option()
+		
+func select_menu_option():
+	if hover_index == 0:
+		hide()
+		get_tree().paused = false
+	
+	if hover_index == 1:
+		get_tree().quit()
+	
+func hover_menu_option(new_index):
+	reset_selected_option(hover_index)
+	hover_index = new_index
+	set_selected_option(hover_index)
 
-func _on_Resume_pressed():
-	hide()
-	get_tree().paused = false
+func reset_selected_option(index):
+	menu_options[index].add_color_override("font_color", Color.white)
 
-func _on_Exit_pressed():
-	get_tree().quit()
+func set_selected_option(index):
+	menu_options[index].add_color_override("font_color", Color.aqua)
